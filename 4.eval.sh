3a2457275e134a20a97835599e68ee758229038a
Nuo Chen <162539463+NuoJohnChen@users.noreply.github.com>
2024-03-07 16:55:44 +0800

    Update 4.eval.sh

--- 4.eval.sh
+++ 4.eval.sh
@@ -7,7 +7,7 @@ mkdir -p $result_folder
 log_name=$(date +"%m-%d_%H-%M").log
 
 python ./src/evaluate/eval_qwen.py \
---input_path=./data/meditron/test.json \
+--input_path=./data/qwen/test.json \
 --output_path=${result_folder}/model_ans.jsonl \
 --score_path=${result_folder}/score.json \
 --wrong_item_path=${result_folder}/wrong_item.json > ${log_folder}/$log_name 2>&1 &
