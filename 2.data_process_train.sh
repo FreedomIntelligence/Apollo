a6a4736f5240c5246ffdffb27432799bcf43cbdb
Nuo Chen <162539463+NuoJohnChen@users.noreply.github.com>
2024-03-07 16:29:08 +0800

    Update 2.data_process_train.sh

--- 2.data_process_train.sh
+++ 2.data_process_train.sh
@@ -1,13 +1,15 @@
-experiment_name=yiallsftcom_Data
+model_name = Qwen
+model_path = /your_model_path/Qwen1.5-0.5B
+experiment_name=qwenallsftcom_data
 log_folder="./logs/${experiment_name}"
 mkdir -p $log_folder
 log_name=$(date +"%m-%d_%H-%M").log
 
 
-python ./src/process/prepare/data_process_train_yi.py \
+python ./src/process/prepare/data_process_train_qwen.py \
 --data_path ./metadata/train/sft.json \
---model_path /your_model_path/Yi-34B \
+--model_path ${model_path} \
 --wandb_log ./wandb_logs \
 --experiment_name ${experiment_name} \
---save_path ./data/Yi/allsftcom > ${log_folder}/$log_name 2>&1 &
+--save_path ./data/${model_name}/allsftcom > ${log_folder}/$log_name 2>&1 &
 
