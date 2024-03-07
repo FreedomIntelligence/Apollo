4fe07f4753c0e2460b228ba3fda439844034ea56
Nuo Chen <162539463+NuoJohnChen@users.noreply.github.com>
2024-03-07 16:29:44 +0800

    Update 4.eval.sh

--- 4.eval.sh
+++ 4.eval.sh
@@ -1,4 +1,4 @@
-experiment_name=meditron70b_test
+experiment_name=Qwen1.5-0.5B_test
 cd .
 log_folder="./logs/${experiment_name}"
 result_folder="./results/${experiment_name}"
@@ -6,7 +6,7 @@ mkdir -p $log_folder
 mkdir -p $result_folder
 log_name=$(date +"%m-%d_%H-%M").log
 
-python ./src/evaluate/eval_meditron70b.py \
+python ./src/evaluate/eval_qwen.py \
 --input_path=./data/meditron/test.json \
 --output_path=${result_folder}/model_ans.jsonl \
 --score_path=${result_folder}/score.json \
